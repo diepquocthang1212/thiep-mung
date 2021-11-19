f1af1d7dd21948b2f79b1c0911e3da86bbcc4ae2
diepquocthang1212 <94687788+diepquocthang1212@users.noreply.github.com>
2021-11-19 18:57:17 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,8 +3,6 @@ const giftUrl = 'http://nodemy.vn';
 const eventName = 'Chúc Mừng 20-11';
 const titleCard = 'Tặng người ấy';
 const contentCard = 'Chúc cô của tụi em 20/11 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với cô trong hôm nay và cả những ngày sau';
-
-
 const giftImage = 'hot-girl.png';
 const base64 = '';
 const giftImageBase64 = "data:image/png;base64, " + base64;
